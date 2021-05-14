827d4dd3dbd8b6b166fd7b7f6f95b875272b8b0e
MurilloFelix <murillo.felix1511@gmail.com>
2021-05-14 17:35:29 -0300

    WIP Module 4 class 3

--- src/pages/Catalog/index.tsx
+++ src/pages/Catalog/index.tsx
@@ -1,8 +1,38 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { Text, View, } from 'react-native';
-import { Container } from './styles'
+import {
+  Container,
+  ProductContainer,
+  ProductList,
+  Product,
+  ProductImage,
+  ProductTitle,
+  PriceContainer,
+  ProductPrice,
+  ProductButton,
+  ProductButtonText,
+ } from './styles'
 
 const Catalog = () => {
+  const [products, setProducts] = useState([{
+    "products": [
+      {
+        "id": "1",
+        "title": "Assinatura Trimestral",
+        "image_url":
+          "https://res.cloudinary.com/robertosousa1/image/upload/v1594492578/dio/quarterly_subscription_yjolpc.png",
+        "price": 150
+      },
+      {
+        "id": "2",
+        "title": "Assinatura Anual",
+        "image_url":
+          "https://res.cloudinary.com/robertosousa1/image/upload/v1594492578/dio/annual_subscription_qyolci.png",
+        "price": 540
+      }
+    ]
+  }])
+
 
   return (
     <Container>
